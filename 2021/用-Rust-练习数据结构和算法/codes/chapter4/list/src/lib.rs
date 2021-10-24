47f8a1837208c71cb59172405592b6af3017d2e4
suhanyujie <suhanyujie@qq.com>
2021-10-24 22:12:49 +0800

    update post

--- 2021/用-Rust-练习数据结构和算法/codes/chapter4/list/src/lib.rs
+++ 2021/用-Rust-练习数据结构和算法/codes/chapter4/list/src/lib.rs
@@ -70,7 +70,21 @@ impl TransactionLog {
         self.length += 1;
         self.tail = Some(new);
     }
+
+    pub fn pop(&mut self) -> Option<String>{
+        self.head.take().map(|head| {
+            if let Some(next) = (*head).borrow_mut().next.take() {
+                self.head = Some(next);
+            } else {
+                self.tail.take();
+            }
+            self.length -= 1;
+            Rc::try_unwrap(head).ok().expect("Something is terribly wrong").into_inner().value
+        })
+    }
+
 }
+
 
 #[cfg(test)]
 mod tests {
